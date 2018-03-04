3164f2eb10ad3404652cd60d75b7e83ac5db7075
Karl-Eiholzer <Karl.Eiholzer@gmail.com>
2018-03-04 11:28:25 -0500

    Moved rounding functions to a separate functions file

--- Project_2/G_33_challenge.py
+++ Project_2/G_33_challenge.py
@@ -1,26 +1,9 @@
 import time
 import math
+from Functions.g_33_functions import *
 
 '''Set parameters'''
 run = 1
-
-'''Define Functions'''
-def yield_round(raw_yield):
-    'Applies Rule G-33 yield rounding for an floating number input'
-    n = float('%.4f'%(raw_yield))
-    n = round(n,2)
-    return n
-
-def dollar_price_round(raw_DP):
-    'Applies Rule G-33 dollar price rounding for an floating number input'
-    n = float('%.3f'%(raw_DP))
-    return n
-
-def accrued_interest_round(raw_AI):
-    'Applies Rule G-33 accrued interest rounding for an floating number input'
-    n = float('%.3f'%(raw_AI))
-    n = round(n,2)
-    return n
 
 def main():
     'Main Control Function'
