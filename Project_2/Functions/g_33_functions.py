1eb8fc84c4715e870687c5e573c9122dde9d08b1
Karl-Eiholzer <Karl.Eiholzer@gmail.com>
2018-03-04 14:49:23 -0500

    Fixed rounding problems
    Also added error handling for users who input letters instead of numbers

--- Project_2/Functions/g_33_functions.py
+++ Project_2/Functions/g_33_functions.py
@@ -1,17 +1,20 @@
 '''Define Functions'''
 def yield_round(raw_yield):
-    'Applies Rule G-33 yield rounding for an floating number input'
-    n = float('%.4f'%(raw_yield))
-    n = round(n,3)
+    'Applies Rule G-33 yield rounding '
+    from decimal import Decimal, ROUND_DOWN, ROUND_HALF_UP
+    n1 = Decimal(raw_yield).quantize(Decimal('.0001'), rounding=ROUND_DOWN)
+    n = Decimal(n1).quantize(Decimal('.001'), rounding=ROUND_HALF_UP)
     return n
 
 def dollar_price_round(raw_DP):
-    'Applies Rule G-33 dollar price rounding for an floating number input'
-    n = float('%.3f'%(raw_DP))
+    'Applies Rule G-33 dollar price rounding '
+    from decimal import Decimal, ROUND_DOWN
+    n = Decimal(raw_DP).quantize(Decimal('.001'), rounding=ROUND_DOWN)
     return n
 
 def accrued_interest_round(raw_AI):
-    'Applies Rule G-33 accrued interest rounding for an floating number input'
-    n = float('%.3f'%(raw_AI))
-    n = round(n,2)
+    'Applies Rule G-33 accrued interest rounding '
+    from decimal import Decimal, ROUND_DOWN, ROUND_HALF_UP
+    n1 = Decimal(raw_AI).quantize(Decimal('.001'), rounding=ROUND_DOWN)
+    n = Decimal(n1).quantize(Decimal('.01'), rounding=ROUND_HALF_UP)
     return n
--- Project_2/G_33_challenge.py
+++ Project_2/G_33_challenge.py
@@ -1,36 +1,71 @@
+'''Import necessary libraries'''
+
 import time
 import math
-from Functions.g_33_functions import *
+from decimal import *
+from Functions.g_33_functions import yield_round, dollar_price_round, accrued_interest_round
 
-'''Set parameters'''
+'''Set loop parameters'''
 run = 1
 
+
+def inputNumber(message):
+  while True:
+    try:
+       userInput = int(input(message))       
+    except ValueError:
+       print("Not an integer! Try again.")
+       continue
+    else:
+       return userInput 
+       break 
+    
 def main():
     'Main Control Function'
-    price_value_raw = raw_input('Enter the number you want formatted: ')
+    print('')
+    '''price_value_raw = raw_input('Enter the number you want formatted: ')'''
+    Var = 1
+    while Var > 0:
+      try:
+        price_value_raw = float(raw_input('Enter the number you want formatted: '))       
+      except ValueError:
+        print('')
+        print("   ***   Not a valid number! Try again.   ***")
+        print('')
+        continue
+      else:
+        Var = Var -1     
     price_value = float(price_value_raw)
-    print('So you want ' + calc_type + ' and ' + str(price_value) + '?  OK')
+    print('')
     # Choose and call correct function
-    if calc_type == 'Y':
+    if calc_type.upper() == 'Y':
         display_Y = yield_round(price_value)
-        print('You have provided a yield')
-        print('Correct display is: ' + str( display_Y ) )
-    elif calc_type == 'D':
+        print('   You have provided a yield of ' + str(price_value) )
+        print('')
+        print('   Correct display is: ' + str( display_Y ) )
+        print('')
+    elif calc_type.upper() == 'D':
         display_DP = dollar_price_round(price_value)
-        print('You have provided a dollar price')
-        print('Correct display is: ' + str( display_DP ) )
-    elif calc_type == 'A':
+        print('   You have provided a dollar price of ' + str(price_value) )
+        print('')
+        print('   Correct display is: ' + str( display_DP ) )
+        print('')
+    elif calc_type.upper() == 'A':
         display_AI = accrued_interest_round(price_value)
-        print('You have provided accrued interest')
-        print('Correct display is: ' + str( display_AI ) )
+        print('   You have provided an accrued interest of ' + str(price_value) )
+        print('')
+        print('   Correct display is: ' + str( display_AI ) )
+        print('')
 
 while run == 1:
     calc_type = raw_input('Enter type of price to input (A, D, Y, or Q): ')
     if calc_type == 'Q' or calc_type == 'q':
+        print('')
         print('You are bored already?')
         print('Good bye!!')
+        print('')
         run = 0
-    elif calc_type == 'A' or calc_type == 'D' or calc_type == 'Y':
+    elif calc_type.upper() == 'A' or calc_type.upper() == 'D' or calc_type.upper() == 'Y':
         main()
     else:
         print('Input not recognized. Try again')
