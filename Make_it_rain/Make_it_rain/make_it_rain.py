03cdc43abfb93147b9d5fd3c1f9ac3e38d576057
Karl-Eiholzer <Karl.Eiholzer@gmail.com>
2019-03-16 12:12:40 -0400

    Adding new Make it Rain project
    Will be a data visualization for trade data

---
+++ Make_it_rain/Make_it_rain/make_it_rain.py
@@ -0,0 +1,20 @@
+'''Import necessary libraries'''
+
+import pygame, sys
+from pygame.locals import *
+pygame.init()
+screen = pygame.display.set_mode((640,480))
+
+'''Set parameters'''
+xpos = 50
+
+'''Main Body'''
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            sys.exit()
+    xpos += 1
+    screen.fill((255,255,255))
+    pygame.draw.circle(screen,(0,255,0),(xpos,200),20)
+    pygame.display.update()
