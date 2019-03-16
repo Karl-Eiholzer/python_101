9d35f5d25c072472f5afc1923f8756aab5ef2b5e
Karl-Eiholzer <Karl.Eiholzer@gmail.com>
2019-03-16 12:43:37 -0400

    Add movement

--- Make_it_rain/make_it_rain.py
+++ Make_it_rain/make_it_rain.py
@@ -14,7 +14,11 @@ while 1:
     for event in pygame.event.get():
         if event.type == QUIT:
             sys.exit()
-    xpos += 1
+    pressed_keys = pygame.key.get_pressed()
+    if pressed_keys[K_RIGHTBRACKET]
+        xpos += 1
+    if pressed_keys[K_LEFTBRACKET]
+        xpos -= 1
     screen.fill((255,255,255))
     pygame.draw.circle(screen,(0,255,0),(xpos,200),20)
     pygame.display.update()
